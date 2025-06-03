22b3685dac0e2e0d9d09f325933601c3e1c893a3
Tim Tepatti <tim@tepatti.com>
2025-06-02 21:55:26 -0400

    Added initial team models and endpoints

--- demo.py
+++ demo.py
@@ -1,14 +1,37 @@
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
+
+class TeamBase(SQLModel):
+    name: str = Field(index=True)
+    headquarters: str
+
+class Team(TeamBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    heroes: list["Hero"] = Relationship(back_populates="team")
+
+class TeamCreate(TeamBase):
+    pass
+
+class TeamPublic(TeamBase):
+    id: int
+
+class TeamUpdate(SQLModel):
+    name: str | None = None
+    headquarters: str | None = None
 
 class HeroBase(SQLModel):
     name: str = Field(index=True)
     secret_name: str
     age: int | None = Field(default=None, index=True)
 
+    team_id: int | None = Field(default=None, foreign_key="team.id")
+
 class Hero(HeroBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str = Field()
+
+    team: Team | None = Relationship(back_populates="heroes")
 
 class HeroCreate(HeroBase):
     password: str
@@ -21,6 +44,8 @@ class HeroUpdate(SQLModel):
     secret_name: str | None = None
     age: int | None = None
     password: str | None = None
+
+    team_id: int | None = None
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -96,5 +121,56 @@ def delete_hero(*, session: Session = Depends(get_session), hero_id: int):
     if not hero:
         raise HTTPException(status_code=404, detail="Hero not found")
     session.delete(hero)
+    session.commit()
+    return {"ok": True}
+
+@app.post("/teams/", response_model=TeamPublic)
+def create_team(*, session: Session = Depends(get_session), team: TeamCreate):
+    db_team = Team.model_validate(team)
+    session.add(db_team)
+    session.commit()
+    session.refresh(db_team)
+    return db_team
+
+@app.get("/teams/", response_model=list[TeamPublic])
+def read_teams(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100)
+):
+    teams = session.exec(select(Team).offset(offset).limit(limit)).all()
+    return teams
+
+@app.get("/teams/{team_id}", response_model=TeamPublic)
+def read_team(*, session: Session = Depends(get_session), team_id: int):
+    team = session.get(Team, team_id)
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    return team
+
+@app.patch("/teams/{team_id}", response_model=TeamPublic)
+def update_team(
+    *,
+    session: Session = Depends(get_session),
+    team_id: int,
+    team: TeamUpdate
+):
+    db_team = session.get(Team, team_id)
+    if not db_team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    team_data = team.model_dump(exclude_unset=True)
+    db_team.sqlmodel_update(team_data)
+    session.add(db_team)
+    session.commit()
+    session.refresh(db_team)
+    return db_team
+
+@app.delete("/teams/{team_id}")
+def delete_team(*, session: Session = Depends(get_session), team_id: int):
+    team = session.get(Team, team_id)
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    session.delete(team)
     session.commit()
     return {"ok": True}
