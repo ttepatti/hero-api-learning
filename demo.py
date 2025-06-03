ffa55662c843cb39c700618188f8c3a289d0fddb
Tim Tepatti <tim@tepatti.com>
2025-06-02 21:34:09 -0400

    Added delete route

--- demo.py
+++ demo.py
@@ -84,3 +84,13 @@ def update_hero(hero_id: int, hero: HeroUpdate):
         session.commit()
         session.refresh(db_hero)
         return db_hero
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        session.delete(hero)
+        session.commit()
+        return {"ok": True}
